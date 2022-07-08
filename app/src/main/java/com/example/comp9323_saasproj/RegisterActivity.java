e43417960a9fd00a9360352a69a1f8e6fb6d2c61
Michael Huang <1525940982@qq.com>
2022-07-08 21:45:35 +1000

    new version with register

--- app/src/main/java/com/example/comp9323_saasproj/RegisterActivity.java
+++ app/src/main/java/com/example/comp9323_saasproj/RegisterActivity.java
@@ -1,30 +1,58 @@
 package com.example.comp9323_saasproj;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 public class RegisterActivity extends AppCompatActivity {
 
     EditText tvStuNumber,tvStuPwd,tvStuConfirmPwd;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+
+    private void createUserStructure(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String name = "Your Name:";
+        String faculty ="Your Faculty";
+        String id = user.getUid();
+        String mailAddress = user.getEmail();
+        String course = "Your Course";
+        UserInformation userInformation = new UserInformation(id,name,faculty,mailAddress,course);
+        databaseReference.child(id).setValue(userInformation);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
         tvStuNumber = findViewById(R.id.et_username);
         tvStuPwd = findViewById(R.id.et_password);
         tvStuConfirmPwd = findViewById(R.id.et_confirm_password);
 
         Button register = findViewById(R.id.buttonRegister);
         Button cancel = findViewById(R.id.buttonCancel);
+
+
         register.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -35,8 +63,9 @@ public class RegisterActivity extends AppCompatActivity {
 //                    user.setPassword(tvStuPwd.getText().toString());
 //                    UserDbHelper dbHelper = new UserDbHelper(getApplicationContext(),UserDbHelper.DB_NAME,null,1);
 //                    dbHelper.addUser(user);
-                    Toast.makeText(RegisterActivity.this,"恭喜你注册成功!",Toast.LENGTH_SHORT).show();
-                    finish();
+                    registerUser();
+//                    Toast.makeText(RegisterActivity.this,"Congratulations! Registration succeeded!",Toast.LENGTH_SHORT).show();
+//                    finish();
                 }
 //                if (v.getId() == R.id.buttonRegister) {
 //                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
@@ -57,25 +86,65 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-
+    private void registerUser(){
+        String email = tvStuNumber.getText().toString().trim();
+        String password= tvStuPwd.getText().toString().trim();
+        System.out.println("here1");
+        System.out.println(email);
+        System.out.println(password);
+//        if(TextUtils.isEmpty(email)){
+//            Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
+//            return;
+//            //email is empty, stopping the function execution
+//        }
+//        if(TextUtils.isEmpty(password)){
+//            Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show();
+//            return;
+//            //email is empty, stopping the function execution
+//        }
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+        System.out.println("here2");
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        System.out.println("here3");
+                        if(task.isSuccessful()) {
+                            //user is successfully reg and logged in
+                            // Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            //progressDialog.cancel();
+                            //execute profile
+                            //startActivity(new Intent(getApplicationContext(), Login.class));
+                            createUserStructure();
+                            Toast.makeText(RegisterActivity.this, "Registered Successfully\nNow you can login", Toast.LENGTH_SHORT).show();
+                            progressDialog.cancel();
+//                            finish();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Registered failed, try again", Toast.LENGTH_SHORT).show();
+                            progressDialog.cancel();
+                        }
+                    }
+                });
+    }
     public boolean CheckInput() {
         String username = tvStuNumber.getText().toString();
         String password = tvStuPwd.getText().toString();
         String confirm_password = tvStuConfirmPwd.getText().toString();
         if(username.trim().equals("")) {
-            Toast.makeText(RegisterActivity.this,"用户名不能为空!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this,"User name cannot be empty!",Toast.LENGTH_SHORT).show();
             return false;
         }
         if(password.trim().equals("")) {
-            Toast.makeText(RegisterActivity.this,"密码不能为空!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this,"Password cannot be empty!",Toast.LENGTH_SHORT).show();
             return false;
         }
         if(confirm_password.trim().equals("")) {
-            Toast.makeText(RegisterActivity.this,"确认密码不能为空!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this,"Confirm password cannot be empty!",Toast.LENGTH_SHORT).show();
             return false;
         }
         if(!password.trim().equals(confirm_password.trim())) {
-            Toast.makeText(RegisterActivity.this,"两次密码输入不一致!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this,"Password input is inconsistent!",Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;

cd353ec2a0d8b5e2e46cd6077bbba77d2eabe666
MSIK <767831805@qq.com>
2022-07-08 16:37:09 +0800

    输入格式 finish

--- app/src/main/java/com/example/comp9323_saasproj/RegisterActivity.java
+++ app/src/main/java/com/example/comp9323_saasproj/RegisterActivity.java
@@ -6,14 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText tvStuNumber,tvStuPwd,tvStuConfirmPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvStuNumber = findViewById(R.id.et_username);
+        tvStuPwd = findViewById(R.id.et_password);
+        tvStuConfirmPwd = findViewById(R.id.et_confirm_password);
 
         Button register = findViewById(R.id.buttonRegister);
         Button cancel = findViewById(R.id.buttonCancel);
@@ -21,10 +29,19 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.buttonRegister) {
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                if(CheckInput()) {
+//                    User user = new User();
+//                    user.setUsername(tvStuNumber.getText().toString());
+//                    user.setPassword(tvStuPwd.getText().toString());
+//                    UserDbHelper dbHelper = new UserDbHelper(getApplicationContext(),UserDbHelper.DB_NAME,null,1);
+//                    dbHelper.addUser(user);
+                    Toast.makeText(RegisterActivity.this,"恭喜你注册成功!",Toast.LENGTH_SHORT).show();
+                    finish();
                 }
+//                if (v.getId() == R.id.buttonRegister) {
+//                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+//                    startActivity(intent);
+//                }
             }
         });
 
@@ -38,5 +55,28 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public boolean CheckInput() {
+        String username = tvStuNumber.getText().toString();
+        String password = tvStuPwd.getText().toString();
+        String confirm_password = tvStuConfirmPwd.getText().toString();
+        if(username.trim().equals("")) {
+            Toast.makeText(RegisterActivity.this,"用户名不能为空!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.trim().equals("")) {
+            Toast.makeText(RegisterActivity.this,"密码不能为空!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(confirm_password.trim().equals("")) {
+            Toast.makeText(RegisterActivity.this,"确认密码不能为空!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!password.trim().equals(confirm_password.trim())) {
+            Toast.makeText(RegisterActivity.this,"两次密码输入不一致!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
